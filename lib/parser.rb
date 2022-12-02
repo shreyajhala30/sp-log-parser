47d356bc57d0205e96c97d0898b465bc1c2e858b
Shreya <shreya.freedom@gmail.com>
2022-12-02 17:58:20 +0530

    add log parser

---
+++ lib/parser.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+class Parser
+  class Error < StandardError; end
+  CHUNK_SIZE_IN_BYTES = 4096
+
+  attr_reader :file_path
+
+  def initialize(file_path:)
+    @file_path = file_path.strip
+
+    raise Error, 'File Not Found' unless File.exist?(file_path)
+    raise Error, 'File Not Readable' unless File.readable?(file_path)
+  end
+
+  def log_entries
+    return {} if File.empty?(file_path)
+
+    generate_log_entries
+  end
+
+  private
+
+  def generate_log_entries
+    return @log_entries if defined?(@log_entries)
+
+    @log_entries = {}
+    file = File.new file_path
+    file.each do |line|
+      url, ip = line.strip.split(' ', 2)
+      @log_entries[url] = (@log_entries[url] || []) << ip
+    end
+    @log_entries
+  rescue StandardError => e
+    raise Error, "Unexpected Processing Failure: #{e.message}"
+  end
+end
---
+++ spec/parser_spec.rb
@@ -0,0 +1,51 @@
+# frozen_string_literal: true
+
+require_relative '../lib/parser'
+
+RSpec.describe Parser do
+  subject(:parser) { Parser.new(file_path:) }
+
+  let(:file_path) { 'spec/data/server.log' }
+
+  context 'validations' do
+    context 'with invalid file path' do
+      let(:file_path) { 'invalid-file-path' }
+
+      it 'validates the passed file exists' do
+        expect { parser }.to raise_error(Parser::Error, 'File Not Found')
+      end
+    end
+
+    context 'with a non readable file' do
+      before { allow(File).to receive(:readable?).with(file_path).and_return(false) }
+
+      it 'validates file is readable' do
+        expect { parser }.to raise_error(Parser::Error, 'File Not Readable')
+      end
+    end
+  end
+
+  describe '#log_entries' do
+    context 'when the file is empty' do
+      let(:file_path) { 'spec/data/empty_file.log' }
+
+      it 'returns an empty hash' do
+        expect(parser.log_entries).to eq({})
+      end
+    end
+
+    context 'when the file contains logs' do
+      let(:expected_log_entires) do
+        {
+          '/about' => ['061.945.150.735', '061.945.150.735'],
+          '/help_page/1' => ['126.318.035.038', '929.398.951.889', '722.247.931.582', '929.398.951.889'],
+          '/about/2' => ['444.701.448.104']
+        }
+      end
+
+      it 'returns hash containing url and its visitors' do
+        expect(parser.log_entries).to eq(expected_log_entires)
+      end
+    end
+  end
+end
